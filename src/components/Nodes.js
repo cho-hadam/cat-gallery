cbfa26dd112776eb6a7a073bbcd05c1fd7ecfbd2
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-05-14 15:50:33 +0900

    이벤트 최적화

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -36,24 +36,25 @@ export default function Nodes({ $app, initialState, onClick, onBackClick }) {
                     </div>
                     ${nodesTemplate}`
         : nodesTemplate;
-
-      this.$target.querySelectorAll(".Node").forEach(($node) => {
-        $node.addEventListener("click", (e) => {
-          const { nodeId } = e.currentTarget.dataset;
-
-          if (!nodeId) {
-            this.onBackClick();
-          }
-
-          const selectedNode = this.state.nodes.find(
-            (node) => node.id === nodeId
-          );
-
-          if (selectedNode) {
-            this.onClick(selectedNode);
-          }
-        });
-      });
     }
   };
+
+  this.$target.addEventListener("click", (e) => {
+    const $node = e.target.closest(".Node");
+
+    if ($node) {
+      const { nodeId } = $node.dataset;
+
+      if (!nodeId) {
+        this.onBackClick();
+        return;
+      }
+
+      const selectedNode = this.state.nodes.find((node) => node.id === nodeId);
+
+      if (selectedNode) {
+        this.onClick(selectedNode);
+      }
+    }
+  });
 }
